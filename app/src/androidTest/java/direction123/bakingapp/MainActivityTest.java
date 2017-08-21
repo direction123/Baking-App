dc33eaa6e832eb8fdbf1ef7d8254c86215210213
direction123 <direction.wfx@gmail.com>
2017-08-20 20:41:40 -0700

    remove unused imports

--- app/src/androidTest/java/direction123/bakingapp/MainActivityTest.java
+++ app/src/androidTest/java/direction123/bakingapp/MainActivityTest.java
@@ -1,15 +1,9 @@
 package direction123.bakingapp;
 
-import android.content.Intent;
-import android.support.test.espresso.ViewInteraction;
 import android.support.test.espresso.intent.Intents;
-import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
-import org.hamcrest.Matchers;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,11 +11,8 @@ import org.junit.runner.RunWith;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
-import static android.support.test.espresso.matcher.ViewMatchers.withParent;
-import static org.hamcrest.core.AllOf.allOf;
 
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed;
