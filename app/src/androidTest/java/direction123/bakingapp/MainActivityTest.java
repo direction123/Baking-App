f5b3c0927bc9e058ab8ed06163d669a6640bd4db
direction123 <direction.wfx@gmail.com>
2017-08-20 19:58:57 -0700

    add main activity test

--- app/src/androidTest/java/direction123/bakingapp/MainActivityTest.java
+++ app/src/androidTest/java/direction123/bakingapp/MainActivityTest.java
@@ -1,19 +1,35 @@
 package direction123.bakingapp;
 
 import android.content.Intent;
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.espresso.intent.Intents;
 import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.hamcrest.Matchers;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
 import static org.hamcrest.core.AllOf.allOf;
 
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed;
+
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+
+import java.lang.InterruptedException;
 /**
  * Created by fangxiangwang on 8/20/17.
  */
@@ -25,11 +41,20 @@ public class MainActivityTest {
             new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void mainActivityTest() {
+    public void recipeListTest() {
+        Intents.init();
 
-        // Click on the RecyclerView item at position 2
-    //    onView(withId(R.id.recyclerview_recipes)).perform(RecyclerViewActions.actionOnItemAtPosition(2, click()));
+        onView(withId(R.id.recyclerview_recipes))
+                .check(matches(isCompletelyDisplayed()));
+        try {
+            //wait 1.5 second for recyclerview loading
+            Thread.sleep(1500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        onView(withId(R.id.recyclerview_recipes)).perform(actionOnItemAtPosition(1, click()));
+        intended(hasComponent(StepListActivity.class.getName()));
 
-    }
-
+        Intents.release();
+     }
 }
